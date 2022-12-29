076bc24850ef83277501796e9a85a3b1ac6d8b3d
Ankit Verma <ankitverma209171@gmail.com>
2022-12-29 08:14:48 +0530

    added user game stats endpoints for chart

--- src/routes/game.js
+++ src/routes/game.js
@@ -3,7 +3,6 @@ const {
   createGame,
   listGame,
   listGames,
-  statistics,
 } = require("../controllers/gameController");
 
 //assign router
@@ -12,6 +11,5 @@ const router = express.Router();
 router.post("/create", createGame);
 router.get("/list/:gameId", listGame);
 router.get("/list", listGames);
-router.get("/statistics", statistics);
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-import { statistics } from "../controllers/userController";
+const { statistics } = require("../controllers/userController");
 
 router.get("/statistics", statistics);
 
-export default router;
+module.exports = router;
